a3ebc0c3de88eb88434c2d3acd50e79f7547f0fd
Jon McManus <due@wxwhatever.com>
2021-04-28 19:15:04 +1000

    Hnngh.

--- src/main/java/noobanidus/mods/carrierbees/entities/BeehemothEntity.java
+++ src/main/java/noobanidus/mods/carrierbees/entities/BeehemothEntity.java
@@ -1,34 +1,58 @@
 package noobanidus.mods.carrierbees.entities;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.goal.FollowParentGoal;
 import net.minecraft.entity.ai.goal.LookAtGoal;
 import net.minecraft.entity.ai.goal.SwimGoal;
-import net.minecraft.entity.passive.PigEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
-import net.minecraft.util.*;
-import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import noobanidus.mods.carrierbees.util.MathHelpers;
 
 import javax.annotation.Nullable;
 
-@SuppressWarnings("ALL")
-public class BeehemothEntity extends AppleBeeEntity implements IRideable {
+public class BeehemothEntity extends AppleBeeEntity {
   public static final DataParameter<Boolean> isSaddled = EntityDataManager.createKey(BeehemothEntity.class, DataSerializers.BOOLEAN);
-  private static final DataParameter<Integer> BOOST_TIME = EntityDataManager.createKey(BeehemothEntity.class, DataSerializers.VARINT);
-  private final BoostHelper field_234214_bx_ = new BoostHelper(this.dataManager, BOOST_TIME, isSaddled);
+
+  public static final double SPEED = 0.4d;
+  public static final float MAX_ANGLE = 90f;
+  public static final float MIN_ANGLE = -90f;
+  private static final float MAX_COS_AMPLITUDE = 0.2f;
+
+  private LivingEntity lastMounted = null;
+
+  private boolean setLast = false;
+  private float lastRotationPitch = -1f;
+  private float lastRotationYaw = -1f;
+
+  private double newPosX;
+  private double newPosY;
+  private double newPosZ;
+  private double newRotationYaw;
+  private double newRotationPitch;
+
+  private int hoverTickOffset;
+
+  @OnlyIn(Dist.CLIENT)
+  private double oldHoverOffset;
+
+  @Override
+  public double getMountedYOffset() {
+    return 2d;
+  }
 
   public BeehemothEntity(EntityType<? extends BeehemothEntity> type, World world) {
     super(type, world);
@@ -44,72 +68,236 @@ public class BeehemothEntity extends AppleBeeEntity implements IRideable {
     return false;
   }
 
+  @Nullable
   @Override
-  public boolean canBeSteered() {
-    return getControllingPassenger() instanceof PlayerEntity;
+  public Entity getControllingPassenger() {
+    return this.getPassengers().isEmpty() ? null : this.getPassengers().get(0);
   }
 
   @Override
-  public Vector3d func_230268_c_(LivingEntity livingEntity) {
-    Direction direction = this.getAdjustedHorizontalFacing();
-    if (direction.getAxis() == Direction.Axis.Y) {
-      return super.func_230268_c_(livingEntity);
-    } else {
-      int[][] aint = TransportationHelper.func_234632_a_(direction);
-      BlockPos blockpos = this.getPosition();
-      BlockPos.Mutable blockpos$mutable = new BlockPos.Mutable();
-
-      for (Pose pose : livingEntity.getAvailablePoses()) {
-        AxisAlignedBB axisalignedbb = livingEntity.getPoseAABB(pose);
-
-        for (int[] aint1 : aint) {
-          blockpos$mutable.setPos(blockpos.getX() + aint1[0], blockpos.getY(), blockpos.getZ() + aint1[1]);
-          double d0 = this.world.func_242403_h(blockpos$mutable);
-          if (TransportationHelper.func_234630_a_(d0)) {
-            Vector3d vector3d = Vector3d.copyCenteredWithVerticalOffset(blockpos$mutable, d0);
-            if (TransportationHelper.func_234631_a_(this.world, livingEntity, axisalignedbb.offset(vector3d))) {
-              livingEntity.setPose(pose);
-              return vector3d;
-            }
-          }
-        }
-      }
-
-      return super.func_230268_c_(livingEntity);
+  public void setPositionAndRotationDirect(double x, double y, double z, float yaw, float pitch, int posRotationIncrements, boolean teleport) {
+    if (!isBeingRidden()) {
+      super.setPositionAndRotationDirect(x, y, z, yaw, pitch, posRotationIncrements, teleport);
     }
+
+    posRotationIncrements += 6;
+    this.newPosX = x;
+    this.newPosY = y;
+    this.newPosZ = z;
+    this.newRotationYaw = (double) yaw;
+    this.newRotationPitch = (double) pitch;
+    this.newPosRotationIncrements = posRotationIncrements;
   }
 
   @Override
   protected void registerData() {
     super.registerData();
     getDataManager().register(isSaddled, true);
-    this.dataManager.register(BOOST_TIME, 0);
   }
 
-  public static AttributeModifierMap.MutableAttribute createAttributes() {
-    return MobEntity.func_233666_p_().createMutableAttribute(Attributes.MAX_HEALTH, 42.0D).createMutableAttribute(Attributes.FLYING_SPEED, 0.6).createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.3).createMutableAttribute(Attributes.ATTACK_DAMAGE, 4.0D).createMutableAttribute(Attributes.FOLLOW_RANGE, 128.0D);
+  public void startAllowFlying(LivingEntity entity) {
+    if (entity instanceof PlayerEntity) {
+      ((PlayerEntity) entity).abilities.allowFlying = true;
+    }
   }
 
-  public float getMountedSpeed() {
-    return 0.4f; // (float) this.getAttributeValue(Attributes.MOVEMENT_SPEED) * 0.225F;
+  public void stopAllowFlying(LivingEntity entity) {
+    if (entity instanceof PlayerEntity) {
+      ((PlayerEntity) entity).abilities.allowFlying = false;
+    }
   }
 
-  public void travelTowards(Vector3d travelVec) {
-    super.travel(travelVec);
-  }
-
-  public boolean boost() {
-    return this.field_234214_bx_.boost(this.getRNG());
-  }
-
-  public void travel(Vector3d travelVector) {
-    this.ride(this, this.field_234214_bx_, travelVector);
+  @Override
+  protected void addPassenger(Entity passenger) {
+    super.addPassenger(passenger);
+    if (!world.isRemote && passenger instanceof LivingEntity) {
+      startAllowFlying((LivingEntity) passenger);
+    }
   }
 
   @Override
   public void tick() {
     super.tick();
-    this.updateBodyPitch();
+
+    Entity rider = getControllingPassenger();
+    if (!world.isRemote() && !isBeingRidden() && lastMounted != null) {
+      onDismount();
+    } else if (rider instanceof LivingEntity) {
+      lastMounted = (LivingEntity) rider;
+
+      prevPosX = getPosX();
+      prevPosY = getPosY();
+      prevPosZ = getPosZ();
+      prevRotationPitch = rotationPitch;
+      prevRotationYaw = rotationYaw;
+
+      if (!world.isRemote() || Minecraft.getInstance().player == lastMounted) {
+        updateMountedServer();
+      } else {
+        updateMountedClient();
+      }
+    }
+  }
+
+  private void onDismount() {
+    if (lastMounted instanceof PlayerEntity) {
+      stopAllowFlying(lastMounted);
+    }
+
+    lastMounted = null;
+  }
+
+  @Override
+  protected boolean canFitPassenger(Entity passenger) {
+    return this.getPassengers().size() == 0;
+  }
+
+  @Override
+  public void removePassengers() {
+    if (!getEntityWorld().isRemote()) {
+      for (Entity p : this.getPassengers()) {
+        if (p instanceof LivingEntity) {
+          stopAllowFlying((LivingEntity) p);
+        }
+      }
+    }
+    super.removePassengers();
+  }
+
+  @Override
+  protected void removePassenger(Entity passenger) {
+    if (!getEntityWorld().isRemote() && getControllingPassenger() == passenger) {
+      onDismount();
+    }
+    super.removePassenger(passenger);
+  }
+
+  protected void updateMountedClient() {
+    if (newPosRotationIncrements > 0) {
+      double x = getPosX() + (newPosX - getPosX()) / newPosRotationIncrements;
+      double y = getPosY() + (newPosY - getPosY()) / newPosRotationIncrements;
+      double z = getPosZ() + (newPosZ - getPosZ()) / newPosRotationIncrements;
+
+      float yaw = MathHelpers.normalizeAngle_180((float) (newRotationYaw - rotationYaw));
+      rotationYaw += yaw / newPosRotationIncrements;
+      rotationPitch += (newRotationPitch - rotationPitch) / newPosRotationIncrements;
+
+      newPosRotationIncrements--;
+
+      setPosition(x, y, z);
+      setRotation(rotationYaw, rotationPitch);
+    }
+
+    move(MoverType.SELF, new Vector3d(0, getHoverOffset(), 0));
+  }
+
+  protected void updateMountedServer() {
+    if (!setLast) {
+      setLast = true;
+      lastRotationYaw = rotationYaw;
+      lastRotationPitch = rotationPitch;
+    }
+
+    if (lastMounted instanceof PlayerEntity) {
+      if (lastMounted instanceof ServerPlayerEntity) {
+        ((ServerPlayerEntity) lastMounted).connection.vehicleFloatingTickCount = 0;
+      }
+    } else {
+      lastMounted.rotationYaw = lastMounted.rotationYawHead;
+      // We have to hardcode this speed because the entity may already have ticked,
+      // so we can't count on it having a predictable speed
+      lastMounted.moveForward = 0.5F;
+    }
+
+    // Rotate broom
+    rotationPitch = MathHelpers.normalizeAngle_180(lastMounted.rotationPitch);
+    rotationYaw = MathHelpers.normalizeAngle_180(lastMounted.rotationYaw);
+
+    // Apply maneuverability modifier
+    float maneuverabilityFactor = 1F - 50f; // - (getModifier(BroomModifiers.MANEUVERABILITY) / 2000F);
+    rotationPitch = rotationPitch * (1F - maneuverabilityFactor) + lastRotationPitch * maneuverabilityFactor;
+    // These if's are necessary to fix rotation when the yaw goes over the border of -180F;+180F
+    if (lastRotationYaw - rotationYaw > 180F) {
+      lastRotationYaw -= 360F;
+    }
+    if (lastRotationYaw - rotationYaw < -180F) {
+      lastRotationYaw += 360F;
+    }
+    rotationYaw = rotationYaw * (1F - maneuverabilityFactor) + lastRotationYaw * maneuverabilityFactor;
+    lastRotationPitch = rotationPitch;
+    lastRotationYaw = rotationYaw;
+
+    // Limit the angle under which the player can move up or down
+    if (rotationPitch > MAX_ANGLE)
+      rotationPitch = MAX_ANGLE;
+    else if (rotationPitch < MIN_ANGLE)
+      rotationPitch = MIN_ANGLE;
+
+    setRotation(rotationYaw, rotationPitch);
+
+    // Handle player movement
+    double pitch = ((rotationPitch + 90) * Math.PI) / 180;
+    double yaw = ((rotationYaw + 90) * Math.PI) / 180;
+
+    double x = Math.sin(pitch) * Math.cos(yaw);
+    double z = Math.sin(pitch) * Math.sin(yaw);
+    double y = Math.cos(pitch);
+
+    /*// Apply speed modifier
+    double playerSpeed = lastMounted.getAttribute(Attributes.MOVEMENT_SPEED).getValue();
+    //playerSpeed += getModifier(BroomModifiers.SPEED) / 100;
+    LivingEntity currentRidingEntity = getControllingPassenger() instanceof LivingEntity ? (LivingEntity) getControllingPassenger() : null;
+    float moveForward = lastMounted.moveForward;
+    playerSpeed *= moveForward;
+
+    // Apply acceleration modifier
+    float slowingFactor = 1F; // - ((getModifier(BroomModifiers.ACCELERATION) + 1F) / 2500F);
+    playerSpeed = playerSpeed * (1D - slowingFactor) + lastPlayerSpeed * slowingFactor;*/
+
+    // Apply levitation modifier
+    /*float levitation = 0.2f; //getModifier(BroomModifiers.LEVITATION);
+    float levitationModifier = 0.2f; //(levitation / BroomModifiers.LEVITATION.getMaxTierValue()) * 1.5F;
+    levitationModifier = Math.max(0.2F, levitationModifier);
+    if (y < 0) {
+      levitationModifier = Math.max(1.0F, levitationModifier);
+    }*/
+
+    // Save last speed (don't take into account water modifier)
+    /*lastPlayerSpeed = playerSpeed;*/
+
+    // Apply water speed
+/*    if (this.inWater) {
+      // Apply a log-scale
+      float waterMovementFactor = 1f; //MathHelper.clamp(          getModifier(BroomModifiers.SWIMMING) / BroomModifiers.SWIMMING.getMaxTierValue(), 0F, 1F);
+      waterMovementFactor = (float) Math.log10(1 + waterMovementFactor * 9);
+      playerSpeed *= waterMovementFactor;
+    }*/
+
+    setMotion(getMotion()
+        .mul(0.1, 0.1, 0.1)
+        .add(x * SPEED /** playerSpeed*/, y * SPEED /** playerSpeed * levitationModifier*/, z * SPEED/* * playerSpeed*/));
+
+    // Update motion on client side to provide a hovering effect
+    if (world.isRemote()) {
+      setMotion(getMotion().add(0, getHoverOffset(), 0));
+    }
+
+    move(MoverType.SELF, getMotion());
+  }
+
+  @Override
+  public boolean onLivingFall(float distance, float damageMultiplier) {
+    // Makes sure the player doesn't get any fall damage when on the broom
+    return false;
+  }
+
+  @Override
+  public boolean canBeRiddenInWater(Entity rider) {
+    return true;
+  }
+
+  public static AttributeModifierMap.MutableAttribute createAttributes() {
+    return MobEntity.func_233666_p_().createMutableAttribute(Attributes.MAX_HEALTH, 42.0D).createMutableAttribute(Attributes.FLYING_SPEED, 0.6).createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.3).createMutableAttribute(Attributes.ATTACK_DAMAGE, 4.0D).createMutableAttribute(Attributes.FOLLOW_RANGE, 128.0D);
   }
 
   @Override
@@ -120,89 +308,25 @@ public class BeehemothEntity extends AppleBeeEntity implements IRideable {
     this.goalSelector.addGoal(9, new SwimGoal(this));
   }
 
-  @Override
+/*  @Override
   protected void setRotation(float yaw, float pitch) {
     this.rotationYaw = yaw % 360f;
     this.rotationPitch = pitch % 360f;
     // super.setRotation(yaw, pitch); ???
-  }
+  }*/
 
   public void writeAdditional(CompoundNBT compound) {
     super.writeAdditional(compound);
-    this.field_234214_bx_.setSaddledToNBT(compound);
+    compound.putBoolean("saddled", dataManager.get(isSaddled));
   }
 
-  /**
-   * (abstract) Protected helper method to read subclass entity data from NBT.
-   */
   public void readAdditional(CompoundNBT compound) {
     super.readAdditional(compound);
-    this.field_234214_bx_.setSaddledFromNBT(compound);
-  }
-
-  @Override
-  protected boolean canBeRidden(Entity entityIn) {
-    return !this.isBeingRidden();
-  }
-
-  public void clearAI() {
-    isJumping = false;
-    navigator.clearPath();
-    setAttackTarget(null);
-    setRevengeTarget(null);
-
-    setMoveForward(0);
-    setMoveVertical(0);
-  }
-
-  @Override
-  public void updateRidden() {
-    super.updateRidden();
-
-/*    Entity entity = getRidingEntity();
-
-    if (entity == null || !entity.isAlive()) {
-      stopRiding();
-      return;
-    }
-
-    setMotion(Vector3d.ZERO);
-    clearAI();
-
-    if (entity instanceof PlayerEntity) {
-      PlayerEntity player = (PlayerEntity) entity;
-      if ((player.isSneaking() && !player.abilities.isFlying)) {
-        stopRiding();
-        return;
-      }
-
-      prevRotationPitch = rotationPitch = player.rotationPitch / 2;
-      rotationYawHead = renderYawOffset = prevRotationYaw = rotationYaw = player.rotationYaw;
-      setRotation(player.rotationYawHead, rotationPitch);
-
-      Vector3d vec3d = new Vector3d(0, getMountedYOffset(), 0);
-
-      if (player.isElytraFlying()) {
-        setFlown(true);
-        vec3d = vec3d.scale(1.5);
-      }
-
-      Vector3d pos = new Vector3d(vec3d.x, 0, vec3d.z).rotateYaw((float) (-player.renderYawOffset * Math.PI / 180f)).add(player.getPosX(), player.getPosY() + vec3d.y, player.getPosZ());
-      setPosition(pos.x, pos.y, pos.z);
-    }*/
-  }
-
-  @Override
-  public void updatePassenger(Entity player) {
-    super.updatePassenger(player);
-/*    Vector3d vec3d = new Vector3d(0, getMountedYOffset(), 0);
-
-    Vector3d pos = new Vector3d(vec3d.x, 0, vec3d.z).rotateYaw((float) (-renderYawOffset * Math.PI / 180f)).add(player.getPosX(), player.getPosY() + vec3d.y, player.getPosZ());
-    setPosition(pos.x, pos.y, pos.z);*/
+    dataManager.set(isSaddled, compound.getBoolean("saddled"));
   }
 
   public ActionResultType func_230254_b_(PlayerEntity p_230254_1_, Hand p_230254_2_) {
-    if (/*this.field_234214_bx_.getSaddled() && */!this.isBeingRidden() && !p_230254_1_.isSecondaryUseActive()) {
+    if (/*dataManager.get(isSaddled) && */!this.isBeingRidden() && !p_230254_1_.isSecondaryUseActive()) {
       if (!this.world.isRemote) {
         p_230254_1_.startRiding(this);
       }
@@ -213,80 +337,18 @@ public class BeehemothEntity extends AppleBeeEntity implements IRideable {
     }
   }
 
-  @Override
-  public boolean canPassengerSteer() {
-    return this.getControllingPassenger() != null;
+  protected double getHoverOffset() {
+    float x = world.getGameTime();
+    float t = hoverTickOffset;
+    double newHoverOffset = Math.cos(x / 10 + t) * Math.cos(x / 12 + t) * Math.cos(x / 15 + t) * MAX_COS_AMPLITUDE;
+
+    double newHoverDifference = newHoverOffset - oldHoverOffset;
+    oldHoverOffset += newHoverDifference;
+
+    return newHoverDifference;
   }
 
-  @Nullable
-  @Override
-  public Entity getControllingPassenger() {
-    return this.getPassengers().isEmpty() ? null : this.getPassengers().get(0);
-  }
-
-/*  public void travel(Vector3d travelVector) {
-    float speed = 0.5f; //getTravelSpeed();
-    if (this.isAlive()) {
-      if (canPassengerSteer()) {
-        LivingEntity livingentity = (LivingEntity) this.getControllingPassenger();
-        double y = travelVector.y;
-        double x = travelVector.x;
-        double z = livingentity.moveForward;
-
-        rotationYawHead = livingentity.rotationYawHead;
-        rotationPitch = livingentity.rotationPitch * 0.5f;
-
-        if (livingentity.moveForward != 0) {
-          y = livingentity.getLookVec().y * speed * 50;
-        }
-        if (livingentity instanceof ServerPlayerEntity) {
-          ((ServerPlayerEntity) livingentity).connection.vehicleFloating = false;
-        }
-        moveRelative(speed, travelVector);
-        move(MoverType.SELF, getMotion());
-        //setMotion(getMotion().scale(0.88f));
-
-        Vector3d motion = getMotion();
-        if (motion.length() < 0.04f) {
-          setMotion(motion.add(0, Math.cos(ticksExisted * 0.1f) * 0.02f, 0));
-        }
-
-        float limbSpeed = 0.4f;
-        float amount = 1f;
-        if (getPosY() - prevPosY < -0.1f) {
-          amount = 0;
-          limbSpeed = 0.2f;
-        }
-
-        prevLimbSwingAmount = limbSwingAmount;
-        limbSwingAmount += (amount - limbSwingAmount) * limbSpeed;
-        limbSwing += limbSwingAmount;
-
-        return;
-      }
-    }
-    super.travel(travelVector);
-  }*/
-
-  @Override
-  protected void addPassenger(Entity p_184200_1_) {
-    super.addPassenger(p_184200_1_);
-/*    if (getControllingPassenger() == p_184200_1_) {
-      clearAI();
-      if (getLeashed()) {
-        clearLeashed(true, true);
-      }
-    }*/
-  }
-
-  /**
-   * Returns the Y offset from the entity's position for any entity riding this one.
-   */
-  public double getMountedYOffset() {
-    return 2.05D;
-  }
-
-  private float currentPitch;
+/*  private float currentPitch;
   private float lastPitch;
 
   @Override
@@ -298,5 +360,5 @@ public class BeehemothEntity extends AppleBeeEntity implements IRideable {
   private void updateBodyPitch() {
     this.lastPitch = this.currentPitch;
     this.currentPitch = Math.max(0.0F, this.currentPitch - 0.24F);
-  }
+  }*/
 }
