c73868b16fe5ab49872ba9de1f37c40c49173f52
Jon McManus <due@wxwhatever.com>
2021-05-07 08:55:00 +1000

    Remove the Royal Jelly advancement.

--- src/main/java/noobanidus/mods/carrierbees/init/ModLang.java
+++ src/main/java/noobanidus/mods/carrierbees/init/ModLang.java
@@ -18,8 +18,8 @@ public class ModLang {
     REGISTRATE.addRawLang("carrierbees.advancements.queen.description", "Promote a Beehemoth into a Queen Beehemoth!");
     REGISTRATE.addRawLang("carrierbees.advancements.root.title", "Carried Away");
     REGISTRATE.addRawLang("carrierbees.advancements.root.title.desc", "No bath-houses included, sorry.");
-    REGISTRATE.addRawLang("carrierbees.advancements.royal_jelly.title", "Aeroplane Royal Jelly");
-    REGISTRATE.addRawLang("carrierbees.advancements.royal_jelly.description", "I like aeroplane jelly!");
+/*    REGISTRATE.addRawLang("carrierbees.advancements.royal_jelly.title", "Aeroplane Royal Jelly");
+    REGISTRATE.addRawLang("carrierbees.advancements.royal_jelly.description", "I like aeroplane jelly!");*/
     REGISTRATE.addRawLang("carrierbees.advancements.steed.title", "Loyal Steed");
     REGISTRATE.addRawLang("carrierbees.advancements.steed.description", "Befriend a Beehemoth and dub it your flying steed.");
     REGISTRATE.addRawLang("entity.carrierbees.beehemoth_queen", "Queen Beehemoth");
