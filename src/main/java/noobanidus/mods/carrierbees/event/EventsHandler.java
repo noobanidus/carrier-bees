59f794da91ab1e29dd18ad689a5c5c2febd34c89
Jon McManus <due@wxwhatever.com>
2021-05-14 10:21:22 +1000

    Fix sidedness issue.

--- src/main/java/noobanidus/mods/carrierbees/event/EventsHandler.java
+++ src/main/java/noobanidus/mods/carrierbees/event/EventsHandler.java
@@ -1,13 +1,14 @@
 package noobanidus.mods.carrierbees.event;
 
 import net.minecraft.util.MovementInput;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputUpdateEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import noobanidus.mods.carrierbees.CarrierBees;
 import noobanidus.mods.carrierbees.init.ModEffects;
 
-@Mod.EventBusSubscriber(modid = CarrierBees.MODID)
+@Mod.EventBusSubscriber(modid = CarrierBees.MODID, value= Dist.CLIENT)
 public class EventsHandler {
   @SubscribeEvent
   public static void onInput(InputUpdateEvent event) {
